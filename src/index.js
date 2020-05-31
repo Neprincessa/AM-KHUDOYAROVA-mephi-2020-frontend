5dc9abc58d5be0b29dd50a7dffb734727b169acf
Anastasia Khudoyarova <asya-iriska@yandex.ru>
2020-06-01 01:33:26 +0300

    [feature]: add routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import UserProfile from "./components/UserProfile";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App} />
+            {/* Оба /roster и /roster/:number начинаются с /roster */}
+            <Route path="/profile" component={UserProfile} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
